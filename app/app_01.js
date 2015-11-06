0e2cd390ad1cf3e86e60c23e5e60298e2ee4d447
Christoph Forster <christoph.forster@gmail.com>
2015-11-06 05:07:13 -0800

    Finalized MVC Pattern

--- app/app_01.js
+++ app/app_01.js
@@ -3,6 +3,14 @@
  */
 angular
     .module("app", [])
-    .controller("myController", function ($scope) {
-        $scope.liste = ["Eier", "Butter", "Brot"];
+    .controller("myController", function ($scope, datenQuelle) {
+        $scope.liste = datenQuelle.getListe();
+    })
+    .factory("datenQuelle", function () {
+        var liste = ["Eier", "Butter", "Brot"];
+        return Object.create({
+            getListe: function () {
+                return liste.slice();
+            }
+        });
     });
