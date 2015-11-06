b471353ab818b0bfd998a66c76403879c622cacd
Christoph Forster <christoph.forster@gmail.com>
2015-11-06 04:47:17 -0800

    First MVC Attempt

---
+++ app/app_01.js
@@ -0,0 +1,8 @@
+/**
+ * Created by devebiz on 11/6/15.
+ */
+angular
+    .module("app", [])
+    .controller("myController", function ($scope) {
+        $scope.liste = ["Eier", "Butter", "Brot"];
+    });
